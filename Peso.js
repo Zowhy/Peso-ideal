4a9fb7b4b7be003e71f2a6db8f352326e7700db4
zowhy <dtrovato@gmail.com>
2023-07-10 15:31:55 -0400

    Mudança do Código

--- Peso.js
+++ Peso.js
@@ -1,15 +1,25 @@
-var peso = parseFloat(prompt("Informe o seu peso (em kg):"));
-var altura = parseFloat(prompt("Infome a sua altura (em metros):"));
-var sexo = prompt("Infome o seu sexo (m para masculino e f para feminino):");
-
-var pesoIdeal;
-if(sexo === 'f'){
-  pesoIdeal = 62.1 * altura -44.7;
-} else if (sexo === 'm'){
-  pesoIdeal = 72.7 * altura -58;
-} else {
-  console.log("Sexo invalido. Execute o programa novamente e informe 'm' para masculiuno ou 'f' para feminino");
+function calcularPeso(peso, altura, sexo) {
+  if(sexo === 'f'){
+    pesoIdeal = 62.1 * altura -44.7;
+  } else if (sexo === 'm'){
+    pesoIdeal = 72.7 * altura -58;
+  } else {
+    console.log("Sexo invalido. Execute o programa novamente e informe 'm' para masculiuno ou 'f' para feminino");
+  }
+  return peso;
 }
-if(pesoIdeal){
+
+function imprimePesoIdeal(peso) {
   console.log("O seu peso ideal é:", pesoIdeal.toFixed(2), "kg.");
-} 
+}
+
+  function main() {
+    var peso = parseFloat(prompt("Informe o seu peso (em kg):"));
+    var altura = parseFloat(prompt("Infome a sua altura (em metros):"));
+    var sexo = prompt("Infome o seu sexo (m para masculino e f para feminino):");
+  
+
+peso = calcularPeso(peso, altura, sexo.toUpperCAse())
+imprimePesoIdeal(peso);
+}
+main();
